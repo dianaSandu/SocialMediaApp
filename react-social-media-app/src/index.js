50614c60a7024dffc8362b485bb2a106d1a5ded7
dina_sandufortech <diana.sandu@fortech.ro>
2020-10-29 11:32:57 +0200

    add firebase config

--- react-social-media-app/src/index.js
+++ react-social-media-app/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from "firebase";
 import reportWebVitals from './reportWebVitals';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCj-IBqCdy3E99whnzQDqxjZ7AsvOaOEY4",
+  authDomain: "socialmediareactapp.firebaseapp.com",
+  databaseURL: "https://socialmediareactapp.firebaseio.com",
+  projectId: "socialmediareactapp",
+  storageBucket: "socialmediareactapp.appspot.com",
+  messagingSenderId: "751637053599",
+  appId: "1:751637053599:web:e08ec3579cdbe4708ae2aa",
+  measurementId: "G-P8NQPFMNT1"
+});
 
 ReactDOM.render(
   <React.StrictMode>
