2eea768c7c441c2d8b0b4a34f5da43be948523cf
dina_sandufortech <diana.sandu@fortech.ro>
2020-11-02 10:14:02 +0200

    add firebase functions folder

---
+++ firebase-functions/functions/index.js
@@ -0,0 +1,26 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello world!");
+});
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // [END addMessageTrigger]
+    // Grab the text parameter.
+    const original = req.query.text;
+    // [START adminSdkAdd]
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({ original: original });
+    // Send back a message that we've succesfully written the message
+    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+    // [END adminSdkAdd]
+});
